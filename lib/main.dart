23a64ae18346ed1c83892531bd2584208829f290
HernandoBun <hernandobun@gmail.com>
2024-11-18 14:42:37 +0700

    add const

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           theme: themeProvider.themeData,
-          home: MainScreen(),
+          home:  const MainScreen(),
         );
       },
     );
